e36331b0eb2f28dfa5593a22606983072f0e6d54
shamim1628 <45328431+shanvari@users.noreply.github.com>
2024-07-19 12:40:33 +0330

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,98 +1,103 @@
 $(document).ready(function() {
-    const signinIcon = $('#signin-icon');
-    const backgroundOverlay = $('#background-overlay');
-    const signinModal = $('#signin-modal');
-  
-    signinIcon.click(function() {
-      backgroundOverlay.show();
-      signinModal.show();
-    });
-  
-    backgroundOverlay.click(function() {
-      backgroundOverlay.hide();
-      signinModal.hide();
-    });
-  
-    $('#req').click(function() {
+  const signinIcon = $('#signin-icon');
+  const backgroundOverlay = $('#background-overlay');
+  const signinModal = $('#signin-modal');
+
+  signinIcon.click(function() {
+    backgroundOverlay.show();
+    signinModal.show();
+  });
+
+  backgroundOverlay.click(function() {
+    backgroundOverlay.hide();
+    signinModal.hide();
+  });
+
+  $('#req').click(function() {
+    $('form').hide();
+    $('#req-form').show();
+    handleReqFormSubmit();
+  });
+
+  $('#verification').click(function() {
+    $('form').hide();
+    $('#verification-form').show();
+    handleVerificationFormSubmit();
+  });
+
+  $('#search-verification-id').click(function() {
+    $('form').hide();
+    $('#verification-fill-form').show();
+    handleVerificationFillFormSubmit();
+  });
+
+  $('#test').click(function() {
       $('form').hide();
-      $('#req-form').show();
-      handleReqFormSubmit();
+      $('#test-form').show();
+      handleTestFormSubmit();
     });
-  
-    $('#verification').click(function() {
+
+
+  $('#interview').click(function() {
       $('form').hide();
-      $('#verification-form').show();
-      handleVerificationFormSubmit();
+      $('#interview-form').show();
+      handleTestFormSubmit();
     });
-  
-    $('#search-verification-id').click(function() {
+
+  $('#interview-search').click(function() {
       $('form').hide();
-      $('#verification-fill-form').show();
+      $('#interview-fill-form').show();
       handleVerificationFillFormSubmit();
     });
 
-    $('#test').click(function() {
-        $('form').hide();
-        $('#test-form').show();
-        handleTestFormSubmit();
-      });
-// ++
-      $('#test-search').click(function() {
-        $('form').hide();
-        $('#test-fill-form').show();
-        handleVerificationFillFormSubmit();
-      });
 
-      $('#interview').click(function() {
-        $('form').hide();
-        $('#interview-form').show();
-        handleTestFormSubmit();
-      });
+  $('#mali').click(function() {
+      $('form').hide();
+      $('#mali-form').show();
+      handleTestFormSubmit();
+    });
 
-      $('#interview-search').click(function() {
-        $('form').hide();
-        $('#interview-fill-form').show();
-        handleVerificationFillFormSubmit();
-      });
+  $('#mali-search').click(function() {
+      $('form').hide();
+      $('#mali-fill-form').show();
+      handleVerificationFillFormSubmit();
+    });
 
-
-      $('#mali').click(function() {
-        $('form').hide();
-        $('#mali-form').show();
-        handleTestFormSubmit();
-      });
-
-      $('#mali-search').click(function() {
-        $('form').hide();
-        $('#mali-fill-form').show();
-        handleVerificationFillFormSubmit();
-      });
-
-      $('#enfagh').click(function() {
-        $('form').hide();
-        $('#enfagh-form').show();
-        handleTestFormSubmit();
-      });
-
-      // $('#enfagh-search').click(function() {
-      //   $('form').hide();
-      //   $('#mali-fill-form').show();
-      //   handleVerificationFillFormSubmit();
-      // });
+  $('#enfagh').click(function() {
+      $('form').hide();
+      $('#enfagh-form').show();
+      handleTestFormSubmit();
+    });
+// Add the following code to display the username after authentication
+  $(document).ready(function() {
+    const sharedData = require('./app').sharedData;
+    window.alert(JSON.stringify(sharedData));
+    $('#username').text("hiiiii");
+    $('#username').text(JSON.stringify(sharedData));
+    });
 
 
-      // $('#test-search').click(function() {
-      //   $('form').hide();
-      //   $('#test-fill-form').show();
-      //   handleVerificationFillFormSubmit();
-      // });
 
-    // $('#trust').click(function() {
+
+    // $('#enfagh-search').click(function() {
     //   $('form').hide();
-    //   $('#trust-form').show();
-    //   handleTrustFormSubmit();
+    //   $('#mali-fill-form').show();
+    //   handleVerificationFillFormSubmit();
     // });
-  
-    // Add similar event listeners for other forms...
-  
-  });
+
+
+    // $('#test-search').click(function() {
+    //   $('form').hide();
+    //   $('#test-fill-form').show();
+    //   handleVerificationFillFormSubmit();
+    // });
+
+  // $('#trust').click(function() {
+  //   $('form').hide();
+  //   $('#trust-form').show();
+  //   handleTrustFormSubmit();
+  // });
+
+  // Add similar event listeners for other forms...
+
+});
