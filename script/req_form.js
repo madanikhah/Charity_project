1e4d0694939d1d6fad378560c3b4880433f42910
shamim1628 <45328431+shanvari@users.noreply.github.com>
2024-07-19 12:43:11 +0330

    Update req_form.js

--- script/req_form.js
+++ script/req_form.js
@@ -1,50 +1,3 @@
-function handleReqFormSubmit() {
-  const form = document.getElementById('req-form');
-
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    // Validate the form data
-    if (!validateFormData(data)) {
-      return; // Return if the data is invalid
-    }
-    const firstname = document.getElementById('firstname-req-id').value;
-    const lastname = document.getElementById('lastname-req-id').value;
-    const fathername = document.getElementById('fathername-req-id').value;
-    const national_code = document.getElementById('national-code-req-id').value;
-    const birth = document.getElementById('birth-req-id').value;
-    const phone = document.getElementById('phone-req-id').value;
-    const representative = document.getElementById('representative-req-id').value;
-    const date = document.getElementById('date-req-id').value;
-    const level = document.getElementById('level-req-id').value;
-
-       try {
-      const response = await fetch('/submit_req_form', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ firstname, lastname,fathername,national_code,birth,phone,representative,date,level })
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        //console.log(data.message);
-        // Redirect the user or perform other 
-        window.alert("ثبت شد");
-        window.location.href = '/';
-      } else {
-      //   const { error } = await response.json();
-        window.alert("مشکلی پیش آمده");
-        // Display an error message to the user
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      // Display a general error message to the user
-    }
-  });
-  }
-
-
   function validateFormData(data) {
     let isValid = true;
   
@@ -113,7 +66,7 @@ function handleReqFormSubmit() {
       }
   
       try {
-        const response = await fetch('/submit_req_form', {
+        const response = await fetch('/submit_request', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -124,7 +77,7 @@ function handleReqFormSubmit() {
         if (response.ok) {
           const data = await response.json();
           window.alert("ثبت شد");
-          window.location.href = '/';
+          window.location.href = '/home';
         } else {
           window.alert("مشکلی پیش آمده");
         }
